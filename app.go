73a4158800a1a67ea7183f374ddd9a8f3c8edb50
dfreire <dario.freire@gmail.com>
2014-08-23 14:34:50 +0100

    added dependency to boltdb

--- app.go
+++ app.go
@@ -1,47 +1,59 @@
 package app
 
 import (
-    "fmt"
+	"fmt"
+    "os"
+	"github.com/boltdb/bolt"
 )
 
 type Domain struct {
-    ids map[string]bool
+    db *bolt.DB
+	ids map[string]bool
 }
 
 func NewDomain() *Domain {
-    return &Domain{ids: make(map[string]bool)}
+	db, err := bolt.Open("app.db", 0600, nil)
+	if err != nil {
+		panic(err)
+	}
+    return &Domain{ db: db, ids: make(map[string]bool)}
+}
+
+func (self *Domain) Close() {
+    self.db.Close()
+    os.Remove("app.db") // TODO
 }
 
 type CreatedAppEvent struct {
-    Id string
+	Id string
 }
 
 func (self *Domain) Create(id string) error {
-    if self.ids[id] {
-        return fmt.Errorf("ID already exists")
-    }
-    event := CreatedAppEvent{Id: id}
-    self.OnCreatedAppEvent(event)
-    return nil
+	if self.ids[id] {
+		return fmt.Errorf("ID already exists")
+	}
+	event := CreatedAppEvent{Id: id}
+	self.OnCreatedAppEvent(event)
+	return nil
 }
 
 func (self *Domain) OnCreatedAppEvent(event CreatedAppEvent) {
-    self.ids[event.Id] = true
+	self.ids[event.Id] = true
 }
 
 type RemovedAppEvent struct {
-    Id string
+	Id string
 }
 
 func (self *Domain) Remove(id string) error {
-    if self.ids[id] != true {
-        return fmt.Errorf("ID does not exist")
-    }
-    event := RemovedAppEvent{Id: id}
-    self.OnRemovedAppEvent(event)
-    return nil
+	if self.ids[id] != true {
+		return fmt.Errorf("ID does not exist")
+	}
+	event := RemovedAppEvent{Id: id}
+	self.OnRemovedAppEvent(event)
+	return nil
 }
 
 func (self *Domain) OnRemovedAppEvent(event RemovedAppEvent) {
-    delete(self.ids, event.Id)
+	delete(self.ids, event.Id)
 }
--- app_test.go
+++ app_test.go
@@ -1,33 +1,35 @@
 package app_test
 
 import (
-    "github.com/puffinframework/app"
-    "testing"
-    "github.com/stretchr/testify/assert"
+	"github.com/puffinframework/app"
+	"github.com/stretchr/testify/assert"
+	"testing"
 )
 
 func TestCreate(t *testing.T) {
-    domain := app.NewDomain()
+	domain := app.NewDomain()
+    defer domain.Close()
 
-    err := domain.Create("app1")
-    assert.Nil(t, err)
+	err := domain.Create("app1")
+	assert.Nil(t, err)
 
-    err = domain.Create("app1")
-    assert.NotNil(t, err)
+	err = domain.Create("app1")
+	assert.NotNil(t, err)
 
-    err = domain.Create("app2")
-    assert.Nil(t, err)
+	err = domain.Create("app2")
+	assert.Nil(t, err)
 }
 
 func TestRemove(t *testing.T) {
-    domain := app.NewDomain()
+	domain := app.NewDomain()
+    defer domain.Close()
 
-    err := domain.Remove("app1")
-    assert.NotNil(t, err)
+	err := domain.Remove("app1")
+	assert.NotNil(t, err)
 
-    err = domain.Create("app1")
-    assert.Nil(t, err)
+	err = domain.Create("app1")
+	assert.Nil(t, err)
 
-    err = domain.Remove("app1")
-    assert.Nil(t, err)
+	err = domain.Remove("app1")
+	assert.Nil(t, err)
 }
